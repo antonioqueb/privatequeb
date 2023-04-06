c5553671e21d1a2a01916255ba37c85d7f544d87
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2023-04-06 15:02:40 +0000

    up

--- django-api-portfolio/project/api/views.py
+++ django-api-portfolio/project/api/views.py
@@ -1,6 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 from project.api.serializers import ProjectSerializer
 from project.models import Project
+from rest_framework.decorators import action
+from rest_framework import status
+from rest_framework.response import Response
 
 from rest_framework.decorators import api_view, authentication_classes
 from rest_framework.authentication import BasicAuthentication
@@ -9,4 +12,9 @@ from rest_framework.authentication import BasicAuthentication
 class ProjectViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-    
+
+    @action(detail=False, methods=['delete'])
+    def delete_all(self, request):
+        projects = Project.objects.all()
+        projects.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
